cbc4092c83a97b6a46b54dbaeae9c6c3e54b4ebc
mzaedan <zaedanm108@gmail.com>
2022-04-08 13:58:07 +0700

    first Commit

---
+++ src/Navbar.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+
+// Class Component (rcc)
+// export default class navbar extends Component {
+//   render() {
+//     return (
+//       <div>
+//           <h2>Navbar</h2>
+//       </div>
+//     )
+//   }
+// }
+
+
+//Function Component (rfc)
+// export default function navbar() {
+//   return (
+//     <div>navbar</div>
+//   )
+// }
+
+
+//Arrow Function (rfac)
+const Navbar = () => {
+  return (
+    <div>
+        <h2>Navbar</h2>
+    </div>
+  )
+}
+
+export default Navbar
+
